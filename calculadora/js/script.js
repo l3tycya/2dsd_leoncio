0ae84c381bed286d8ea85c7431d11b4ace8a64a3
l3tycya <leticordeiro08@gmail.com>
2024-10-03 15:22:29 +0000

    iniciando o javascript

---
+++ calculadora/js/script.js
@@ -0,0 +1,19 @@
+//faz referencia aos elemento dom//
+const numero1 = document.querySelector('#numero1')
+const numero2 = document.querySelector('#numero2')
+const btncalcular = document.querySelector('#btn-calcular')
+
+const resultado = document.querySelector('.resultado')
+const divisao = document.querySelector('.divisao')
+const adicao = document.querySelector('.adicao')
+const subtracao = document.querySelector('.subtracao')
+const multiplicacao = document.querySelector('.multiplicacao')
+
+
+//cria a função calcular//
+
+
+
+
+
+//adiciona escutador de eventos//
