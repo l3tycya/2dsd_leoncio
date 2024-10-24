2a08dbfe17e184967a7dcceede5484b89730e857
l3tycya <leticordeiro08@gmail.com>
2024-10-24 14:53:50 +0000

    a gente fez o botao de limpeza

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -1,32 +1,30 @@
 
-// FAZ REFERÊNCIA AOS ELEMENTOS DOM
-
-const numero1 = document.querySelector('#numero1')
-const numero2 = document.querySelector('#numero2')
-const btnCalcular = document.querySelector('#btn-calcular')
-const resultado = document.querySelector('.resultado')
-const adicao = document.querySelector('.adicao')
-const subtracao = document.querySelector('.subtracao')
-const multiplicacao = document.querySelector('.multiplicacao')
-const divisao = document.querySelector('.divisao')
-
-//alert(numero1)
-// CRIA A FUNÇÃO CALCULAR
-  function calcular(){
+function calcular(){
   const n1 = Number(numero1.value)
   const n2 = Number(numero2.value)
-  if(typeof n1 === 'number'
-  && typeof n2 === 'number' ) {
-  const adicao =  `A soma de ${n1} e ${n2} = ${n1+n2}`
-  //alert(adicao)
-  }else {
-  alert('Por favor, digite um número correto')
+  if(typeof n1 === 'number' && typeof n2 === 'number') {
+
+    const add = `O Resultado da Adição ${n1} + ${n2} = ${n1+n2}`
+      adicao.innerHTML = add
+      //alert(adicao)
+
+      const sub = `O Resultado da Subtração ${n1} - ${n2} = ${n1-n2}`
+      subtracao.innerHTML = sub
+      //alert(subtracao)
+
+      const mul = `O Resultado da Multiplicação ${n1} x ${n2} = ${n1*n2}`
+      multiplicacao.innerHTML = mul
+      //alert(multipliacacao)
+
+      const div = `O Resultado da Divisão ${n1} / ${n2} = ${n1/n2}`
+      divisao.innerHTML = div
+      //alert(divisao)
+  } else{
+      alert('Por favor, digeite um número legitimo')
+  }
 }
-   const adicao =  `A soma de ${n1} e ${n2} = ${n1+n2}`
-    //alert('Resultado: ' + resultado)
-}
-// ADICIONA ESCUTAR DE EVENTOS
-btnCalcular.addEventListener('click', function(evento){
-  evento.preventDefault()//tirar o comportamento padrão
+//Adiciona Esculta de Evento
+btnCalcular.addEventListener('click', function (evento){
+  evento.preventDefault() //Tirar o comportamento padrão
   calcular()
 })
